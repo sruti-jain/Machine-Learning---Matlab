989d3c5e75dcc85ee7d11bb7db318f927b0d82de
sruti-jain <sruti.j1806@gmail.com>
2016-11-28 04:15:59 -0600

    Feature mapping function

---
+++ Logistic_Regression_Regularization/mapFeature.m
@@ -0,0 +1,12 @@
+function out = mapFeature(X1, X2)
+% MAPFEATURE Feature mapping function to polynomial features
+
+degree = 6;
+out = ones(size(X1(:,1)));
+for i = 1:degree
+    for j = 0:i
+        out(:, end+1) = (X1.^(i-j)).*(X2.^j);
+    end
+end
+
+end
