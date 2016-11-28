c89efee9721c4d1e1f2996ffe9a49280ff6a9c9b
sruti-jain <sruti.j1806@gmail.com>
2016-11-28 04:11:16 -0600

    Plot Data

---
+++ Linear_Regression_One_Variable/plotData.m
@@ -0,0 +1,12 @@
+function plotData(x, y)
+
+%PLOTDATA Plots the data points x and y into a new figure 
+% CODE HERE =================================================
+plot(x,y,'rx','MarkerSize', 10);
+xlabel('Population')
+ylabel('Revenue')
+title('Population and revenue plot')
+figure; 
+% ============================================================
+
+end
