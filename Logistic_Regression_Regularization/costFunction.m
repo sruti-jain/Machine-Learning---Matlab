5038a81b524b34717674f4407a0887ff3a10cfc0
sruti-jain <sruti.j1806@gmail.com>
2016-11-28 04:15:24 -0600

    Cost Function

---
+++ Logistic_Regression_Regularization/costFunction.m
@@ -0,0 +1,25 @@
+function [J, grad] = costFunction(theta, X, y, lambda)
+%COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
+
+% CODE HERE ==========================================================
+% Initialize values
+m = length(y); % number of training examples
+J = 0;
+grad = zeros(size(theta));
+templog(:,1) = log(sigmoid(X*theta));
+templog(:,2) = log(1-(sigmoid(X*theta)));
+tempy(:,1) = y;
+tempy(:,2) = 1-y;
+temp = templog.*tempy;
+
+% Formula for cost function. Note theta0 not involved in reglarization
+J = (1/m)*(-sum(temp(:,1))-sum(temp(:,2))) + (lambda/(2*m))*sum(theta(2:end,1).^2);
+
+% Compute gradient- theta0 not calclated with regularization
+grad(1,1) = (1/m)*sum((sigmoid(X*theta)-y).*X(:,1)); 
+grad(2:end,1)=((1/m)*((sigmoid(X*theta)-y)'*X(:,2:end)))'+(lambda/m)*theta(2:end);
+
+grad = grad(:);
+% =============================================================
+
+end
