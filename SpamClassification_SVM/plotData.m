9f8a9919838f2ade1d57d3c8c9732d1f276a2780
sruti-jain <sruti.j1806@gmail.com>
2017-01-02 17:45:25 -0600

    Update plotData.m

--- SpamClassification_SVM/plotData.m
+++ SpamClassification_SVM/plotData.m
@@ -1,11 +1,5 @@
 function plotData(X, y)
-%PLOTDATA Plots the data points X and y into a new figure 
-%   PLOTDATA(x,y) plots the data points with + for the positive examples
-%   and o for the negative examples. X is assumed to be a Mx2 matrix.
-%
-% Note: This was slightly modified such that it expects y = 1 or y = 0
 
-% Find Indices of Positive and Negative Examples
 pos = find(y == 1); neg = find(y == 0);
 
 % Plot Examples
