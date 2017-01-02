6d8293b4ec9df1dc68725db17af83f773fed65b2
sruti-jain <sruti.j1806@gmail.com>
2017-01-02 17:48:42 -0600

    Update svmPredict.m

--- SpamClassification_SVM/svmPredict.m
+++ SpamClassification_SVM/svmPredict.m
@@ -1,16 +1,6 @@
 function pred = svmPredict(model, X)
-%SVMPREDICT returns a vector of predictions using a trained SVM model
-%(svmTrain). 
-%   pred = SVMPREDICT(model, X) returns a vector of predictions using a 
-%   trained SVM model (svmTrain). X is a mxn matrix where there each 
-%   example is a row. model is a svm model returned from svmTrain.
-%   predictions pred is a m x 1 column of predictions of {0, 1} values.
-%
 
-% Check if we are getting a column vector, if so, then assume that we only
-% need to do prediction for a single example
 if (size(X, 2) == 1)
-    % Examples should be in rows
     X = X';
 end
 
